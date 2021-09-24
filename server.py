51bd6536c805449dd045ad3a355ed155ec1691dc
Devin <devin.m.bushey@gmail.com>
2021-09-24 11:45:19 -0700

    Working Server - Part 4 complete

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 import socket
 import sys
 
-BUF_SIZE = 164
+BUF_SIZE = 172
 HOST = '10.21.75.71'
 PORT = 12345
 
@@ -12,12 +12,42 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((HOST, PORT))
 sock.listen(1)
 print('Server:', sock.getsockname())
+dictionary = { }
 while True:
     sc, sockname = sock.accept()
     print('Client:', sc.getpeername())
-    data = sc.recv(BUF_SIZE)
-    result = data.decode(encoding='UTF-8', errors='strict')
-    print('Recieved',result[:-1])
+
+    try:
+        data = sc.recv(BUF_SIZE)
+        result = data.decode(encoding='UTF-8', errors='strict').strip()
+        header = result[:3]
+        key = result[3:11:1]
+
+        if header == 'PUT':
+            message = result[11:]
+            if len(message) > 160:
+                print('Error: message exceeds 160 bytes')
+                raise Exception('')
+
+            dictionary[key] = message
+            put = 'OK\n'
+            data = put.encode()
+            print('OK')
+
+        elif header == 'GET':
+            get = 'Key: ' + key + ' Message: ' + dictionary[key] + '\n'
+            data = get.encode()
+            print('Key:', key, 'Message:', dictionary[key])
+
+        else:
+            print('Error: must be GET or PUT')
+            raise Exception('')
+            
+    except Exception as err:
+        error = 'NO\n'
+        data = error.encode()
+        print(err)
+
     sc.sendall(data)
     sc.close()
 
