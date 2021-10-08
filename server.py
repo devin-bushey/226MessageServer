68e6f03b913b3ece5266ae0e238e4043d46e2515
Devin <devin.m.bushey@gmail.com>
2021-10-08 10:10:57 -0700

    Complete server.py with successful multisession test

--- server.py
+++ server.py
@@ -1,151 +1,187 @@
 #!/usr/bin/python3
 
 import socket
-import sys
+import threading
+import logging
 
+KEY_SIZE = 8
+MAX_MSG_SIZE = 160
 BUF_SIZE = 1024
-HOST = ''   #empty string = ALL
+
+ERROR_RESPONSE = b"NO"
+GET_CMD = "GET".encode('utf-8')
+HOST = ''
+NUM_CONNECTIONS = 5
+OK_RESPONSE = b'OK'
 PORT = 12345
-NUM_OF_CONNECTIONS = 1
+PUT_CMD = "PUT".encode('utf-8')
 
-# PURPOSE:
-# Create a TCP Server
-# RETURN:
-# Returns a socket
-def createSocket():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((HOST, PORT))
-    sock.listen(NUM_OF_CONNECTIONS)
-    print('Server:', sock.getsockname())
-    return sock
+lock = threading.Semaphore(1)
+messages = {}
 
+#
 # PURPOSE:
-# Validates message length. Must be between 0-160 bytes
+# Given a valid socket connection, reads in bytes from the connection until
+# either a newline is encountered or BUF_SIZE charcters have been read,
+# whichever occurs first
+#
 # PARAMETERS:
-# msg = string
-# RETURN:
-# bool
-def checkMsgLen(msg):
-    if (len(msg) <= 0 or len(msg) > 160):
-        print('Error: message must be between 0-160 bytes')
-        return False
-    return True
+# 'current_socket' contains a valid server socket
+#
+# RETURN/SIDE EFFECTS:
+# Returns the bytes that have been read in
+#
+# NOTES:
+# No connection errors are handled
+#
+def get_line(current_socket):
+        buffer = b''
+        size = 0
+        while True:
+                data = current_socket.recv(1)
+                size += 1
+                if data == b'\n' or size >= BUF_SIZE:
+                        return buffer
+                buffer = buffer + data
 
+#
 # PURPOSE:
-# Validate GET data. 
-# GET request cannot include a message
-# Checks if there is a message with the associated key
+# Given a string, extracts the key and message from it
+#
 # PARAMETERS:
-# key, message = string
-# dictionary = dictionary containing key/value pairs for the key/message
-# RETURN:
-# bool
-def checkGET(key, message, dictionary):
-    if (len(message) != 0):
-        print('Error: GET cannot include message')
-        return False
-    if not bool(dictionary.get(key)):
-        print('Error: Message not found')
-        return False
-    return True
+# 's' is the string that will be used for the key and message extraction
+#
+# RETURN/SIDE EFFECTS:
+# Returns (key, message, flag), where flag is True if the extraction
+# succeeded, False otherwise
+#
+# NOTES:
+# To succeed, the string must be of format "KEYMSG" where KEY is of length KEY_SIZE
+#
+def get_key(s):
+    if len(s) < KEY_SIZE:
+        return ("", "", False)
+    else:
+        lock.acquire()
+        result = (s[:KEY_SIZE], s[KEY_SIZE:], True)
+        lock.release() 
+        return result
 
+#
 # PURPOSE:
-# Process the PUT request by validating the data and encoding the success/fail message
+# Given a string, extracts the key and message, and stores the message in messages[key]
+#
 # PARAMETERS:
-# message = string
-# RETURN:
-# returns the encoded OK message if data is valid, otherwise raises an exception
-def processPUT(message):
-    if not checkMsgLen(message):
-        raise Exception('NO\n')
-    print('OK')
-    put = 'OK\n'
-    data = put.encode()
-    return data
+# 's' is the string that will be used for the key and message extraction
+#
+# RETURN/SIDE EFFECTS:
+# Returns OK_RESPONSE on success, ERROR_RESPONSE otherwise
+#
+# NOTES:
+# To succeed, the string must be of format "KEYMSG" where KEY is of length KEY_SIZE
+# and MSG does not exceed MAX_MSG_SIZE
+#
+def process_put(s):
 
+    (key, msg, ok) = get_key(s)
+    if (not ok) or (len(msg) > MAX_MSG_SIZE):
+        return ERROR_RESPONSE
+
+    #print("Saving", msg, "with key", key)
+    lock.acquire()
+    messages[key] = msg
+    lock.release()
+    return OK_RESPONSE
+
+#
 # PURPOSE:
-# Process the GET request by validating the data and encoding the message that is assocuated with the key
+# Given a string, extracts the key and message from it, and returns message[key]
+#
 # PARAMETERS:
-# key = string, 8 byte alphanumeric
-# message = string, 160 bytes
-# dictionary = dictionary containing key/value pairs for the key/message
-# RETURN:
-# returns the encoded message if data is valid, otherwise raises an exception
-def processGET(key, message, dictionary):
-    if not checkGET(key, message, dictionary):
-        raise Exception('\n')
-    print('Key:', key, 'Message:', dictionary[key])
-    get = dictionary[key] + '\n'
-    data = get.encode()
-    return data
+# 's' is the string that will be used for the key and message extraction
+#
+# RETURN/SIDE EFFECTS:
+# Returns the message if the extraction succeeded, and b'' otherwise
+#
+# NOTES:
+# To succeed, the string must be of format "KEY" where KEY is of length KEY_SIZE
+#
+def process_get(s):
+    (key, msg, ok) = get_key(s)
+    lock.acquire()
+    keyInMsg = key in messages
+    lock.release()
 
+    if not ok or len(msg) != 0 or not keyInMsg:
+        return b''
+
+    #print("Found", messages[key], "with key", key)
+    lock.acquire()
+    result = messages[key]
+    lock.release()
+
+    return result
+
+#
 # PURPOSE:
-# encode an error message
+# Given a string, parses the string and implements the contained PUT or GET command
+#
 # PARAMETERS:
-# err = error message that is created from an Exception
-# RETURN:
-# return encoded error message
-def processError(err):
-    err = str(err)
-    data = err.encode()
-    print(err)
-    return data
+# 's' is the string that will be used for parsing
+#
+# RETURN/SIDE EFFECTS:
+# Returns the result of the command if the extraction succeeded, ERROR_RESPONSE otherwise
+#
+# NOTES:
+# The string is assumed to be of format "CMDKEYMSG" where CMD is either PUT_CMD or GET_CMD,
+# KEY is of length KEY_SIZE, and MSG varies depending on the command. See process_put(s)
+# and process_get(s) for details regarding what the commands do and their return values
+#
+def process_line(s):
+    if s.startswith(PUT_CMD):
+        return process_put(s[(len(PUT_CMD)):])
+    elif s.startswith(GET_CMD):
+        return process_get(s[(len(GET_CMD)):])
+    else:
+        return ERROR_RESPONSE
 
+#
 # PURPOSE:
-# Send encoded data to the client and terminate the socket
-# PARAMETERS:
-# socket = opened socket
-# data = encoded data that is ready to be sent to the client
-# RETURN
-# void
-def sendData(socket, data):
-    socket.sendall(data)
-    socket.close()
+#
+def handle_client(sc, sockname):
+    print('[New Connection:] ', sockname)
+    print('[Active Connections:] ', threading.activeCount() - 1)
 
-# PURPOSE:
-# run a basic message server that expects either a PUT or GET command followed by a 8-byte alphanumeric
-# key, followed by an optional newline-terminated string (if the command is PUT)
-# The PUT command stores the message and its associated key in memory and returns a status message to the client
-# The GET command retrieves the message in memory that is associated with the given key and returns it to the client
-# PARAMETERS:
-# sock contains the socket assocated with the server
-# RETURN:
-# void
-def runBasicMessageServer(sock):
-    dictionary = { }
-    while True:
-        sc, sockname = sock.accept()
-        print('Client:', sc.getpeername())
-
+    connected = True
+    while connected:
         try:
-            data = sc.recv(BUF_SIZE)
-            data_decoded = data.decode(encoding='UTF-8', errors='strict').strip()
+            data = get_line(sc)
+            #print('Data:', data)
+            response = process_line(data)
+            sc.sendall(response + b'\n')
+            connected = False
+        except socket.error as err:
+            print('Socket Error: ', err)
+            sc.close()
 
-            header = data_decoded[:3]
-            key = data_decoded[3:11]
-            message = data_decoded[11:]
+    sc.close()
 
-            if header == 'PUT':
-                data = processPUT(message)
-                dictionary[key] = message
-            elif header == 'GET':
-                data = processGET(key, message, dictionary)
-            else:
-                print('Error: must be GET or PUT')
-                raise Exception('NO\n')
-                
-        except Exception as err:
-            data = processError(err)
+#
+# Note: Connection errors are not handled
+#
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((HOST, PORT))
+sock.listen(NUM_CONNECTIONS)
+print('Server:', sock.getsockname())
 
-        try:
-            sendData(sc, data)
-        except Exception as err:
-            print(err)
+while True:
+    sc, sockname = sock.accept()
+    #print('Client:', sc.getpeername())
+    thread = threading.Thread(target = handle_client, args = (sc, sockname))
+    thread.start()
 
-def main():
-    sock = createSocket()
-    runBasicMessageServer(sock)
 
-if __name__ == "__main__":
-    main()
+
+
+
